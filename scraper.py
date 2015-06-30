5a64a35fd9e5f5bd91c2bcc6e189c5bd0cdc1423
byrnli <liam.byrne@abs.gov.au>
2015-06-30 13:45:21 +1000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,15 +1,26 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+html = scraperwiki.scrape("http://abs.gov.au/AUSSTATS/abs@.nsf/mf/1345.0")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root = lxml.html.fromstring(html)
+
+
+root.cssselect("tr valign="top"")
+    record={}
+    record["indicator"]=td[0].text_content()
+    record["pub"]=td[1].text_content()
+    record["period"]=td[2].text_content()
+    record["unit"]=td[3].text_content()
+    record["value"]=td[4].text_content()
+    record["changeyr"]=td[6].text_content()
+    scraperwiki.sqlite.save(unique_keys=['indicator'], data=record)
+
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
